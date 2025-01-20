b997d5f4f574414ce7d6f7c79e611e77e878d1f5
ellietownsend <epaigetownsend@gmail.com>
2025-01-19 22:07:55 -0800

    Add files via upload

---
+++ c++helper.h
@@ -0,0 +1,16 @@
+//
+//  c++helper.cpp
+//  catchUp
+//
+//  Created by Ellie Townsend on 1/12/25.
+//
+
+#include <stdio.h>
+// dot operator: an object that we created (from a struct/class) . the name of a member (attribute) of that type
+/* what goes on the left is an OBJECT created from a class */
+
+// pointer: a pointer to an object of a struct type -> the name of a member (attribute) of that type
+/* what goes on the left is a POINTER to an OBJECT of a struct/class */
+
+// double semicolin: the name of a struct type :: the name of a member of that type, with the purpose of connecting that strut type to a member we have created
+/* what goes on the left is the NAME of the class */
