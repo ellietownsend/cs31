1772681a2b5fc3dce55e4d3d34131646c026f0e1
ellietownsend <epaigetownsend@gmail.com>
2025-02-03 11:25:14 -0800

    Add files via upload
    /* Write a program that inputs an intefer that represents a lenth of time in seconds. The program should then output the number of hours, minuites, and seconds that corresponds to that number in seconds. For example, if the user inputs 50,391 total seconds then the program should output 13 hours, 59 minitues and 51 seconds*/

---
+++ time.h
@@ -0,0 +1,17 @@
+//
+//  time.hpp
+//  bookCH1
+//
+//  Created by Ellie Townsend on 2/3/25.
+//
+
+#ifndef time_hpp
+#define time_hpp
+
+#include <stdio.h>
+/* Write a program that inputs an intefer that represents a lenth of time in seconds. The program should then output the number of hours, minuites, and seconds that corresponds to that number in seconds. For example, if the user inputs 50,391 total seconds then the program should output 13 hours, 59 minitues and 51 seconds*/
+
+void timeCalc();
+
+
+#endif /* time_hpp */
